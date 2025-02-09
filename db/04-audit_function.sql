cd2b62a7a9ad4e48189a435f330796c77a5d097e
Roman Tsyupryk <cooprin@gmail.com>
2025-02-09 22:14:32 +0100

    audit8

--- db/04-audit_function.sql
+++ db/04-audit_function.sql
@@ -1,28 +1,42 @@
-DO $$ 
-BEGIN 
-    IF EXISTS (
-        SELECT 1 FROM pg_proc p
-        JOIN pg_namespace n ON p.pronamespace = n.oid
-        WHERE n.nspname = 'audit' 
-        AND p.proname = 'log_table_change'
-    ) THEN
-        DROP FUNCTION audit.log_table_change CASCADE;
-    END IF;
-END $$;
-
-CREATE FUNCTION audit.log_table_change()
+CREATE OR REPLACE FUNCTION audit.log_table_change()
 RETURNS TRIGGER AS $$ 
+DECLARE
+    acting_user_id UUID;
+    client_ip INET;
 BEGIN 
+    -- Try to get user_id from the record itself if it's a user table
+    IF TG_TABLE_NAME = 'users' THEN
+        acting_user_id := CASE 
+            WHEN TG_OP = 'DELETE' THEN OLD.id
+            ELSE NEW.id
+        END;
+    ELSE
+        -- Try to get from session setting
+        BEGIN
+            acting_user_id := current_setting('audit.user_id', false)::uuid;
+        EXCEPTION WHEN OTHERS THEN
+            acting_user_id := '00000000-0000-0000-0000-000000000000'::uuid;
+        END;
+    END IF;
+
+    -- Try to get client IP
+    BEGIN
+        client_ip := current_setting('request.client_ip', false)::inet;
+    EXCEPTION WHEN OTHERS THEN
+        client_ip := NULL;
+    END;
+
     INSERT INTO audit.audit_logs(
-        user_id,  -- Додаємо user_id
+        user_id,
         action_type,
         entity_type,
         entity_id,
         old_values,
-        new_values
+        new_values,
+        ip_address
     ) VALUES (
-        current_setting('audit.user_id', true)::uuid,  -- Отримуємо user_id з session setting
-        TG_OP,  
+        acting_user_id,
+        TG_OP,
         TG_TABLE_NAME,
         CASE 
             WHEN TG_OP = 'DELETE' THEN OLD.id
@@ -37,7 +51,8 @@ BEGIN
             WHEN TG_OP = 'INSERT' OR TG_OP = 'UPDATE'
             THEN to_jsonb(NEW)
             ELSE NULL
-        END
+        END,
+        client_ip
     );
 
     IF TG_OP = 'DELETE' THEN 
